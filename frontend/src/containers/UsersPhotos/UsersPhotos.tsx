389a98cb7c0b7b8d680fa3e43ec7a350c81f1a42
nelli gorlova <nelli7035@gmail.com>
2025-02-22 17:43:49 +0600

    Added a button to create new images only for the creator of the images

--- frontend/src/containers/UsersPhotos/UsersPhotos.tsx
+++ frontend/src/containers/UsersPhotos/UsersPhotos.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { deletePhoto, fetchPhotosThunk, getUsersPhotos } from '../../features/photos/photosThunk.ts';
 import { selectUser } from '../../features/users/UsersSlice.ts';
@@ -15,6 +15,7 @@ const UsersPhotos = () => {
   const userPhotos = useAppSelector(selectUsersPhotos);
   const user = useAppSelector(selectUser);
   const isLoading = useAppSelector(selectIsLoading);
+  const navigate = useNavigate();
 
   const [selectedPhoto, setSelectedPhoto] = useState<{ image: string, title: string } | null>(null);
 
@@ -23,8 +24,6 @@ const UsersPhotos = () => {
       dispatch(getUsersPhotos(id));
     }
   }, [dispatch, id]);
-
-  console.log(userPhotos);
 
   const onDelete = async (id: string) => {
     await dispatch(deletePhoto(id));
@@ -39,8 +38,12 @@ const UsersPhotos = () => {
     setSelectedPhoto({image, title});
   };
 
+  const handlePhoto = () => {
+    navigate('/photoCards/new');
+  };
 
-  return  (
+
+  return (
     <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg p-5">
       {isLoading ? (
         <PreLoader/>
@@ -48,9 +51,20 @@ const UsersPhotos = () => {
         <p className="text-center">No photos here yet :(</p>
       ) : (
         <>
-        {user?.displayName === userPhotos[0]?.username.displayName && (
-          <h3 className="text-center text-3xl my-5">{user.displayName} gallery</h3>
-        )}
+          <div className="flex justify-between items-center mb-5">
+            {user?.displayName === userPhotos[0]?.username.displayName && (
+              <h3 className="text-center text-3xl my-5">{user.displayName} gallery</h3>
+            )}
+            {user?._id === id && (
+              <button
+                onClick={handlePhoto}
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+              <span
+                className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">Add new Photo</span>
+              </button>
+            )}
+          </div>
+
           <div className="grid grid-cols-3">
             {userPhotos.map((photo) => (
               <div key={photo._id} className="container mx-auto px-4 mb-5">
@@ -65,20 +79,20 @@ const UsersPhotos = () => {
                 />
               </div>
 
-                ))}
-              </div>
-              </>
-              )}
+            ))}
+          </div>
+        </>
+      )}
 
-            {selectedPhoto && (
-              <DialogWindow
-            image={selectedPhoto.image}
-            title={selectedPhoto.title}
-            onClose={onCloseWindow}
-          />
-        )}
-        </div>
-      );
-      };
+      {selectedPhoto && (
+        <DialogWindow
+          image={selectedPhoto.image}
+          title={selectedPhoto.title}
+          onClose={onCloseWindow}
+        />
+      )}
+    </div>
+  );
+};
 
-      export default UsersPhotos;
+export default UsersPhotos;
